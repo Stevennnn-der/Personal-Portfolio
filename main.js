744e3046e56b70bc7f77d2f01e3a2ac761f1d359
Steven012820041010 <68784611+Steven012820041010@users.noreply.github.com>
2023-04-27 12:30:21 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const top_container = document.getElementById("top-container");
+const hourE1 = document.getElementById("hour");
+const minuteE1 = document.getElementById("minutes");
+const secondE1 = document.getElementById("seconds");
+const ampmE1 = document.getElementById("ampm");
+
+window.addEventListener("scroll", ()=>{
+    updateImage();
+});
+
+function updateImage(){
+    top_container.style.opacity = 1 - window.pageYOffset / 700;
+    top_container.style.backgroundSize = 160 - window.pageYOffset/20 + "%";
+}
+
+
+updateClock();
+
+function updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (h > 12) {
+        h -= 12;
+        ampm = "PM";
+    }
+    
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourE1.innerText = h + ":";
+    minuteE1.innerText = m + ":";
+    secondE1.innerText = s;
+    ampmE1.innerText = ampm;
+    setTimeout(() => {
+        updateClock();
+    }, 1000);
+}
+
+
